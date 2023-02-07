43d66e6d176c542d721a7c894efd446cdcb29041
Pongsapat_1022 <pongsapat.inta@kmutt.acth>
2023-02-07 17:04:19 +0700

    HW1_Ex1

---
+++ First/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FunctionF
+{
+    class Program
+    {
+          static double Calculatefuntion(double x)
+        {
+            if (x * x <= 4)
+                return 2;
+                
+            else
+                return 1 + Calculatefuntion(x - (4 * (Math.Abs(x) / x) ) );
+
+        }
+        static void Main(string[] args)
+        {   double x,output;
+            Console.Write("Input number x = ");
+            x = double.Parse(Console.ReadLine());
+
+            output = Calculatefuntion(x);
+            Console.WriteLine("f({0}) = {1}",x,output);
+        }
+    }
+}
